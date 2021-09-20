815ebfcd316247ad476b79f8335af77f4bbd02c7
Ilham AJ <ilhamaryasuta@gmail.com>
2021-09-20 09:53:16 +0700

    new commit

---
+++ Scripts/Autoload/Utils.gd
@@ -0,0 +1,13 @@
+extends Node
+
+static func clamp_vec2_to_mag(vec: Vector2, dir: Vector2, min_mag: float, max_mag: float) -> Vector2:
+	var ans := dir
+	var mag: float = vec.length()
+	var angle: float = abs(vec.angle_to(dir))
+	if angle > PI * 0.5:
+		mag *= -1
+	if vec == Vector2.ZERO:
+		return Vector2.ZERO
+	mag = clamp(mag, min_mag, max_mag)
+	ans *= mag
+	return ans
---
+++ Scripts/fish.gd
@@ -0,0 +1,149 @@
+class_name Fish, "res://fish.png"
+
+extends KinematicBody2D
+
+enum AIState {
+	IDLING,
+	STOP_TO_TURN,
+	TURNING,
+	SWIMMING,
+	BITING,
+}
+
+export(float) var distance_limit = 3
+export(float) var max_speed = 100.0
+export(float) var acceleration = 100.0
+
+export(float) var min_rotate = 0.5
+export(float) var max_rotate_speed = 200.0
+export(float) var rot_acc = 3.0
+
+var current_state: int = 0
+
+var velocity: Vector2 = Vector2.ZERO
+var start_move_pos: Vector2 = Vector2.ZERO
+var start_move_dir: Vector2 = Vector2.ZERO
+var dist_max_to_zero: float = 0.0
+var time_max_to_zero: float = 0.0
+
+var rotate_speed: float = 0.0
+var start_rot_vec: Vector2 = Vector2.RIGHT
+var rot_max_to_zero: float = 0.0
+var time_rot_to_zero: float = 0.0
+
+onready var screen_size: Vector2 = get_viewport_rect().size
+onready var target_pos: Vector2 = screen_size * 0.5
+onready var target_pos_old: Vector2 = screen_size * 0.5
+
+func _ready() -> void:
+	max_rotate_speed = deg2rad(max_rotate_speed)
+	min_rotate = deg2rad(min_rotate)
+
+	time_max_to_zero = max_speed / acceleration
+	dist_max_to_zero = .5 * acceleration * time_max_to_zero * time_max_to_zero
+	
+	time_rot_to_zero = max_rotate_speed / rot_acc
+	rot_max_to_zero = .5 * rot_acc * time_rot_to_zero * time_rot_to_zero
+	
+func _draw() -> void:
+	draw_line(Vector2.ZERO, velocity.rotated(-rotation) / max_speed * 100, Color.red, 3)
+	draw_arc(Vector2.ZERO, 20, 0, rotate_speed / max_rotate_speed * PI, 16, Color.green, 3)
+
+func _process(_delta: float) -> void:
+	update()
+
+func _physics_process(delta: float) -> void:
+	var current_dir: Vector2 = global_transform.basis_xform(Vector2.RIGHT)
+	var target_dir: Vector2 = target_pos - global_position
+	var target_angle: float = current_dir.angle_to(target_dir)
+	var dist: float = target_dir.length()
+	
+	if abs(target_angle) > abs(min_rotate) and dist > distance_limit:
+		if current_state != AIState.TURNING:
+			reset_starting_point()
+			current_state = AIState.STOP_TO_TURN
+	elif dist > distance_limit:
+		if current_state != AIState.SWIMMING and current_state != AIState.STOP_TO_TURN:
+			reset_starting_point()
+		current_state = AIState.SWIMMING
+	else:
+		current_state = AIState.IDLING
+
+	if target_pos_old != target_pos:
+		reset_starting_point()
+	
+	ai(start_move_pos, target_dir, current_dir, target_angle, delta)
+	target_pos_old = target_pos
+
+func reset_starting_point() -> void:
+	start_rot_vec = global_transform.basis_xform(Vector2.RIGHT)
+	start_move_pos = global_position
+	start_move_dir = (target_pos - global_position).normalized()
+	rotate_speed = 0
+
+func ai(start_pos: Vector2, target_dir: Vector2, cur_dir: Vector2, angle: float, delta: float) -> void:
+	var dist: float = target_dir.length()
+	var dist_from_start: float = (target_pos - start_pos).length()
+	var speed: float = velocity.length()
+	var dir: Vector2 = target_dir.normalized()
+	var velocity_dir: Vector2 = velocity.normalized() if velocity != Vector2.ZERO else dir
+	
+	angle = min(angle, 2 * PI - angle)
+	var rot_from_start: float = start_rot_vec.angle_to(target_dir)
+
+	match current_state:
+		AIState.IDLING:
+			velocity = Vector2.ZERO
+			rotate_speed = 0.0
+			return
+		AIState.STOP_TO_TURN:
+			rotate_speed = 0.0
+			if speed <= distance_limit:
+				current_state = AIState.TURNING
+			velocity -= velocity_dir * acceleration * delta
+		AIState.TURNING:
+			velocity = Vector2.ZERO
+			if abs(angle) < rotate_speed * delta:
+				rotation = dir.angle()
+			else:
+				var rot_acc_ing: bool = true
+				if abs(rot_from_start) < rot_max_to_zero * 2:
+					if abs(angle) < abs(rot_from_start) * .5:
+						rot_acc_ing = false
+				elif abs(angle) < rot_max_to_zero:
+					rot_acc_ing = false
+				
+				if rot_acc_ing:
+					rotate_speed += rot_acc * delta * sign(rot_from_start)
+				else:
+					rotate_speed -= rot_acc * delta * sign(rot_from_start)
+				
+				rotate_speed = clamp(abs(rotate_speed), 0.5, max_rotate_speed) * sign(rotate_speed)
+				rotate(rotate_speed * delta)
+			return
+		AIState.SWIMMING:
+			rotate_speed = 0.0
+			var accelerating: bool = true
+			
+			if dist_from_start < dist_max_to_zero * 2:
+				if dist < dist_from_start * .5:
+					accelerating = false
+			elif dist < dist_max_to_zero:
+				accelerating = false
+			
+			if accelerating:
+				velocity += velocity_dir * acceleration * delta
+			else:
+				velocity -= velocity_dir * acceleration * delta
+		_:
+			return
+
+	velocity = Utils.clamp_vec2_to_mag(velocity, cur_dir, 0, max_speed)
+	var _coll = move_and_collide(velocity * delta)
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		var touch_event:= event as InputEventScreenTouch
+		if touch_event.pressed:
+			target_pos.x = clamp(touch_event.position.x, 0, screen_size.x)
+			target_pos.y = clamp(touch_event.position.y, 0, screen_size.y)
